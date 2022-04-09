344dc2218944f0065bbb07d0e1e7a5e9de5bb9e8
Lwazi Mabota <greninja71@gmail.com>
2022-04-08 22:29:39 -0700

    Built function that allows players to add marks on specfic spots on board

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-const gameBoard = (function() {
-    let board = 
-    ['X','O','X','O','X','O','X','O','X','O','X'];
+const gameBoard = (() => {
+    let board = ['X','O','X','O','X','O','X','O','X','O','X'];
+    
     function displayBoard(){
-      for(i = 0; i < board.length; i++){
-          const tic = document.querySelector('.cell');
-          const tic1 = document.querySelector('.cell1');
-          const tic2 = document.querySelector('.cell2');
-          const tic3 = document.querySelector('.cell3');
-          const tic4 = document.querySelector('.cell4');
-          const tic5 = document.querySelector('.cell5');
-          const tic6 = document.querySelector('.cell6');
-          const tic7 = document.querySelector('.cell7');
-          const tic8 = document.querySelector('.cell8');
+        for(i = 0; i < board.length; i++){
+            const tic = document.querySelector('.cell');
+            const tic1 = document.querySelector('.cell1');
+            const tic2 = document.querySelector('.cell2');
+            const tic3 = document.querySelector('.cell3');
+            const tic4 = document.querySelector('.cell4');
+            const tic5 = document.querySelector('.cell5');
+            const tic6 = document.querySelector('.cell6');
+            const tic7 = document.querySelector('.cell7');
+            const tic8 = document.querySelector('.cell8');
+          }
+      }
+      displayBoard();
+  
+    const boardBoxes = document.querySelectorAll("#display").forEach(e => e.addEventListener('click',playerMovement));
+    var XO = "X";
 
-          tic.innerHTML = board[0];
-          tic1.innerHTML = board[1];
-          tic2.innerHTML = board[2];
-          tic3.innerHTML = board[5];
-          tic4.innerHTML = board[6];
-          tic5.innerHTML = board[7];
-          tic6.innerHTML = board[8];
-          tic7.innerHTML = board[9];
-          tic8.innerHTML = board[10]
-
+    function playerMovement (){
+        this.innerHTML = XO;
+        XO = (XO == "X" ? "O" : "X")
     }
-}
-displayBoard()
-    const playerGame = (name, marker) => {
-        const sayHello = () => console.log('hello!');
-        return { name,marker,sayHello
-        };
 
-    };
-    return {
-        displayBoard,
-        playerGame,
-    };
+    /*
+    const createPlayer = (name,marker) => {
+           var XO = "X";
+        return {name,marker}
+    }
+    const lwazi = createPlayer('lwazi','X')
+    const sophia = createPlayer('sophia','O')
+*/
 })();
 
-const player1 = playerGame('Lwazi', 'X');
-const player2 = playerGame('Oliva', 'O');
